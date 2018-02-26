b05f16183078ba34cdd895b27e4b6ff3e63786b1
Gerardo Zúñiga Leal <monodeth@gmail.com>
2018-02-26 16:57:37 -0300

    Include css

--- src/App/App.js
+++ src/App/App.js
@@ -10,6 +10,9 @@ import BarChart from './BarChart'
 import LineChart from './LineChart'
 import Table from './Table'
 import data from './data/dummy.json'
+
+import './App.css'
+
 class App extends Component {
 
   static propTypes = {
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,12 @@ module.exports = {
   module: {
     rules: [
       { test: /\.js$/, exclude: /(node_modules|bower_components)/, use: 'babel-loader' },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
     ]
   }
 }
